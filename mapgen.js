15bc98d72feeb696d30a14f4be6b6d2888d70136
lhskree <loganhsobczak@gmail.com>
2015-01-28 21:51:37 -0500

    added a caraaaazy amount of things

--- mapgen.js
+++ mapgen.js
@@ -1,7 +1,12 @@
 // Clears the canvas
 function clear() {
-    c.fillStyle = "#FFFFFF";
-    c.fillRect(0, 0, WIDTH, HEIGHT);
+  c.fillStyle = "#FFFFFF";
+  c.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+// Returns a random range
+function randRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /*********************************/
@@ -9,20 +14,30 @@ function clear() {
 
 // Sets up the canvas
 var canvas = document.getElementById("canvas"),
-    c = canvas.getContext("2d"),
-    HEIGHT = canvas.height,
-    WIDTH = canvas.width,
-    tileHeight = 10,
-    tileWidth = 10;
+  c = canvas.getContext("2d"),
+  HEIGHT = canvas.height,
+  WIDTH = canvas.width,
+  tileHeight = 2,
+  tileWidth = 2;
 
 // Available Terrains
 var terrains = [];
-terrains.push(new Terrain("water", "#0000FF"));
-terrains.push(new Terrain("mountains", "#364411"));
-terrains.push(new Terrain("forest", "#33FF44"));
+terrains.push(new Terrain("water", "#1c2d81", 0.99, {min : 0.20, max : 0.43}));
+terrains.push(new Terrain("tropical waters", "#324396", 0.98, {max : 0.25}));
+terrains.push(new Terrain("mountains", "#364411", 0.55, {max : 0.1}));
+terrains.push(new Terrain("charaparal", "coral", 0.7, {max : 0.24}));
+terrains.push(new Terrain("lush plains", "#61d44f", 0.88, {max : 0.17}));
+terrains.push(new Terrain("plains", "#47c134", 0.87, {min : 0.15, max : 0.37}));
+terrains.push(new Terrain("forest", "#33FF44",0.6, {min : 0.1, max : 0.40}));
+terrains.push(new Terrain("desert", "beige", 0.75, {max : 0.08}));
+terrains.push(new Terrain("tundra", "#221edd", 0.89, {min : 0.4, max : 0.5}));
+terrains.push(new Terrain("icy waters", "434d7e", 0.9, {min : 0.43, max : 0.5}));
+terrains.push(new Terrain("ice caps", "white", 0.99, {min : 0.47, max : 0.5}));
 
 var worldMap = new World(HEIGHT / tileHeight, WIDTH / tileWidth, tileWidth, tileHeight, terrains, c);
 
 // Make the map!
 clear();
-worldMap.generateMap();
+for (var i = 0; i < 3500; i++) {
+	worldMap.generateMap();
+}
