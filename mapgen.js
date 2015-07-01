1b251d64141ab5d63be954fc3b91294b9460c4e5
lhskree <loganhsobczak@gmail.com>
2015-06-30 20:42:24 -0400

    changed the colors of some of the terrain tiles

--- mapgen.js
+++ mapgen.js
@@ -22,21 +22,21 @@ var canvas = document.getElementById("canvas"),
 
 // Available Terrains
 var terrains = [];
-terrains.push(new Terrain("water", "#1c2d81", .99, {min : 0.20, max : 0.43}, "water", 1));
-terrains.push(new Terrain("warm waters", "#1d2d93", 0.97, {max : 0.33}, "water", 1));
-terrains.push(new Terrain("coral waters", "#0e1f88", 0.97, {min : 0.05, max : 0.2}, "water", 1));
-terrains.push(new Terrain("shallow waters", "#0f0e99", 0.97, {max : 0.3}, "water", 1));
-terrains.push(new Terrain("tropical waters", "#324396", 0.98, {max : 0.25}, "water", 1));
-terrains.push(new Terrain("mountains", "#364411", 0.55, {max : 0.1}, "land", 1));
-terrains.push(new Terrain("dry hills", "#63e55f", 0.78, {max : 0.17}, "land", 1));
-terrains.push(new Terrain("charaparal", "coral", 0.7, {max : 0.24}, "land", 1));
+terrains.push(new Terrain("water", "#1437E4", .99, {min : 0.20, max : 0.43}, "water", 1));
+terrains.push(new Terrain("warm waters", "#2344E6", 0.97, {max : 0.33}, "water", 1));
+terrains.push(new Terrain("coral waters", "#1462E4", 0.97, {min : 0.05, max : 0.2}, "water", 1));
+terrains.push(new Terrain("shallow waters", "#1C41F6", 0.97, {max : 0.3}, "water", 1));
+terrains.push(new Terrain("tropical waters", "#0E60DC", 0.98, {max : 0.25}, "water", 1));
+terrains.push(new Terrain("mountains", "#616A69", 0.85, {max : 0.1}, "land", 1));
+terrains.push(new Terrain("dry hills", "#63e55f", 0.88, {max : 0.17}, "land", 1));
+terrains.push(new Terrain("charaparal", "#DEDC5C", 0.89, {max : 0.24}, "land", 1));
 terrains.push(new Terrain("lush plains", "#61d44f", 0.88, {max : 0.17}, "land", 1));
 terrains.push(new Terrain("verdent plains", "#72e33a", 0.79, {min : 0.05, max : 0.19}, "land", 1));
-terrains.push(new Terrain("plains", "#47c134", 0.87, {min : 0.15, max : 0.37}, "land", 1));
-terrains.push(new Terrain("forest", "#33FF44",0.6, {min : 0.1, max : 0.40}, "land", 1));
-terrains.push(new Terrain("thick forest", "#44ee33",0.85, {min : 0.07, max : 0.41}, "land", 1));
+terrains.push(new Terrain("plains", "#47c134", 0.99, {min : 0.15, max : 0.35}, "land", 1));
+terrains.push(new Terrain("forest", "#33FF44",0.78, {min : 0.1, max : 0.35}, "land", 1));
+terrains.push(new Terrain("thick forest", "#44ee33",0.95, {min : 0.07, max : 0.40}, "land", 1));
 terrains.push(new Terrain("desert", "beige", 0.75, {max : 0.08},"land", 0.9));
-terrains.push(new Terrain("tundra", "#221edd", 0.89, {min : 0.4, max : 0.46}, "land", 1));
+terrains.push(new Terrain("tundra", "#221edd", 0.89, {min : 0.4, max : 0.45}, "land", 1));
 terrains.push(new Terrain("icy waters", "434d7e", 0.9, {min : 0.43, max : 0.48},"water", 1));
 terrains.push(new Terrain("ice caps", "white", 0.99, {min : 0.45, max : 0.5}, "water", 1));
 
